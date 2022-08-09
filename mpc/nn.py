b3496dc85062676504c8b6cb14553a571e01b012
csidrane <csidrane@stanford.edu>
2022-08-08 23:05:47 -0700

    parallel data collection beginning

--- mpc/nn.py
+++ mpc/nn.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
 import time
+from multiprocessing import Pool, cpu_count
 
 
 class PolicyCloningModel(torch.nn.Module):
@@ -101,14 +102,24 @@ class PolicyCloningModel(torch.nn.Module):
             x_train[:, dim] = torch.Tensor(n_pts).uniform_(*self.state_space[dim])
 
         # Now get the expert's control input at each of those points
-        u_expert = torch.zeros((n_pts, self.n_control_dims))
-        data_gen_range = tqdm(range(n_pts))
-        data_gen_range.set_description("Generating training data...")
-        t1 = time.time()
-        for i in data_gen_range:
-            u_expert[i, :] = expert(x_train[i, :])
-        mpc_time = (time.time() - t1)/len(data_gen_range)
-        print("\n Avg. execution time for MPC was: (milliseconds) ", mpc_time*1000)
+
+        # # serial implementation
+        # u_expert = torch.zeros((n_pts, self.n_control_dims))
+        # data_gen_range = tqdm(range(n_pts))
+        # data_gen_range.set_description("Generating training data...")
+        # t1 = time.time()
+        # for i in data_gen_range:
+            # u_expert[i, :] = expert(x_train[i, :])
+        # mpc_time = (time.time() - t1)/len(data_gen_range)
+        # print("\n Avg. execution time for MPC was: (milliseconds) ", mpc_time*1000)
+
+        # parallel implementation
+        # TODO: change to using pathos and dill
+        # https://stackoverflow.com/questions/8804830/python-multiprocessing-picklingerror-cant-pickle-type-function
+        data = torch.tensor_split(x_train, cpu_count(), dim=0)
+        with Pool(cpu_count()) as p:
+            u_expert = list(tqdm(p.imap(expert, data)))
+
 
         # Save it to a file
         torch.save(x_train, save_path + '_examples.pt')
